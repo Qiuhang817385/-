6b25e5979caecdd76efa6d6295c9bfeea607c110
Qiuhang817385 <826266129@qq.com>
2020-04-13 21:50:07 +0800

    表逻辑封装

--- bike/src/components/BaseForm/BaseForm.js
+++ bike/src/components/BaseForm/BaseForm.js
@@ -1,9 +1,60 @@
 import React from 'react'
-
-export default function BaseForm () {
-  return (
-    <>
-      BaseForm
-    </>
-  )
+import { Input, Select, Form, Button, CheckBox, Radio } from 'antd';
+import utils from '../../utils/utils'
+const FormItem = Form.Item;
+const Option = Select.Option;
+export default class BaseForm extends React.Component {
+  // 表逻辑封装
+  formItemList = (type, name, label, placeholder, list, initialValues, width) => {
+    let form = {
+      "select": <FormItem
+        name={name}
+        label={label}>
+        <Select
+          placeholder={placeholder}
+          style={{ width: width }}>
+          {/* 获取表逻辑结构 */}
+          {utils.getOptionList(list)}
+        </Select>
+      </FormItem>
+      ,
+    }
+    return form[type];
+  }
+  initFormList = () => {
+    let arr = []
+    // getFieldValue
+    // getFieldsValue
+    // 值
+    // const getField = this.props.form;
+    // 结构
+    const formList = this.props.formList;
+    console.log('formList :', formList);
+    if (formList && formList.length) {
+      formList.forEach((item, index) => {
+        let name = item.name || '';
+        let type = item.type;
+        let label = item.label;
+        let placeholder = item.placeholder;
+        let list = item.list;
+        let initialValues = item.initialValues || '';
+        let width = item.width;
+        arr.push(this.formItemList(type, name, label, placeholder, list, initialValues, width));
+      })
+    }
+    console.log('arr :', arr);
+    return arr;
+  }
+  render () {
+    return (
+      <Form initialValues={{ ...this.props.initValue }} layout="inline">
+        {/* 动态生成表单 */}
+        {
+          this.initFormList().map((item) => {
+            return item
+          })
+        }
+      </Form>
+    )
+  }
 }
--- bike/src/pages/order/Order.js
+++ bike/src/pages/order/Order.js
@@ -1,11 +1,47 @@
 import React, { useState, useEffect } from 'react'
 import { Card, Button, Table, Form, Select, Modal, message, DatePicker } from 'antd';
-import axios from '../../axios/axios'
+import axios from '../../axios/axios';
+import BaseForm from './../../components/BaseForm/BaseForm'
 import { columns, formList, params, formItemLayout, rowSelection } from './data'
 import './order.scss'
 export default function Order () {
   const [list, setList] = useState([]);
-  const [pagination, setpagination] = useState(null)
+  const [pagination, setpagination] = useState(null);
+  const initValue = {
+    city: '0',
+    order_status: '0'
+  }
+  const formList = [
+    {
+      name: 'city',
+      type: 'select',
+      label: '城市',
+      placeholder: '全部',
+      list: [
+        { id: '0', name: '全部' },
+        { id: '1', name: '北京' },
+        { id: '2', name: '河北' }
+      ],
+      initialValues: '0',
+      width: 100
+    },
+    {
+      type: 'timeComponent',
+    },
+    {
+      name: 'order_status',
+      type: 'select',
+      label: '订单状态',
+      placeholder: '全部',
+      list: [
+        { id: '0', name: '全部' },
+        { id: '1', name: '进行中' },
+        { id: '2', name: '结束' }
+      ],
+      initialValues: '0',
+      width: 100
+    },
+  ]
   useEffect(() => {
     requestList().then((res) => {
       console.log('res :', res);
@@ -22,14 +58,16 @@ export default function Order () {
     })
   }, [])
   let openOrderDetail = () => {
-
   }
   let handleConfirm = () => {
-
   }
   return (
     <>
       <Card className="card-wrapper">
+        <BaseForm
+          formList={formList}
+          initValue={initValue}
+        ></BaseForm>
       </Card>
       <Card className="card-wrapper" style={{ marginTop: 10 }}>
         <Button onClick={openOrderDetail}>
