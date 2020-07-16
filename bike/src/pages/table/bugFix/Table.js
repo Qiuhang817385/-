2f07f302a0a67b879f214963bb81436b6240e14b
Qiuhang817385 <826266129@qq.com>
2020-07-16 23:56:14 +0800

    fix : 修复了表格的复选Bug

--- bike/src/pages/table/bugFix/Table.js
+++ bike/src/pages/table/bugFix/Table.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './style.scss'
 import { columns2 } from './columns';
 import { Card, Spin, Table, Button, Modal, message } from 'antd'
@@ -17,9 +17,10 @@ export default function TableFix () {
     * 多选
     */
   const [selectedRowKeys, setSelectedRowKeys] = useState([]); //keys
+
+  const nowRowKeys = useRef(selectedRowKeys);
+
   const [selectItem, setselectItem] = useState([]); //Item
-  console.log('selectedRowKeys', selectedRowKeys)
-  console.log('selectItem', selectItem)
   const checkMore = {
     type: 'checkbox',
     // 细节,第一个参数必须是selectedRowKeys
@@ -30,26 +31,44 @@ export default function TableFix () {
       setselectItem([...selectedRow]);
     }
   }
+  useEffect(() => {
+    nowRowKeys.current = selectedRowKeys
+  })
   /**
   * 复选表格点击方法
   */
   let handleClick = (rec, index) => {
     // console.log('rec :', rec);//{id: 1, username: "赵敏", sex: 2, state: 3, interest: 4, …}
-    let existIdx = selectedRowKeys.indexOf(rec.key)
+    // 这里的关键点,要取到之前的值   这里每次刷新之后,值都是空
+
+    console.log('selectedRowKeys', nowRowKeys.current);
+
+    let existIdx = nowRowKeys.current.indexOf(rec.key);
     if (existIdx >= 0) {
+      console.log('删除');
       //删除
       setSelectedRowKeys(pre => {
         const dep = JSON.parse(JSON.stringify(pre));
-        return dep.splice(existIdx, 1)
+        dep.splice(existIdx, 1)
+        return dep;
       });
       setselectItem(pre => {
         const dep = JSON.parse(JSON.stringify(pre));
-        return dep.splice(existIdx, 1)
+        dep.splice(existIdx, 1)
+        return dep
       });
     } else {//增加
       //表格内，所有被选中数据的，key的集合
-      setSelectedRowKeys(pre => pre.push(rec.key));
-      setselectItem(pre => pre.push(rec));
+      setSelectedRowKeys(pre => {
+        const dep = JSON.parse(JSON.stringify(pre));
+        dep.push(rec.key)
+        return dep
+      });
+      setselectItem(pre => {
+        const dep = JSON.parse(JSON.stringify(pre));
+        dep.push(rec)
+        return dep;
+      });
       //表格内，所有被选中数据的，集合
     }
   }
